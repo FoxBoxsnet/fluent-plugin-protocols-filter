dfc5071eb3ff48a6c81f634d5be5c99fd49a07bf
foxboxsnet <naokiaoyama2009@gmail.com>
2017-04-29 20:18:26 +0900

    add

--- lib/fluent/plugin/filter_protocols.rb
+++ lib/fluent/plugin/filter_protocols.rb
@@ -24,7 +24,7 @@ module Fluent::Plugin
 
       es.each do |time, record|
         unless record[@key_port].nil?
-          record[@key_prefix] = getprotocolname(record[@key_port], record[@key_proto]) rescue nil
+          record[@key_prefix] = getprotocolname(record[@key_port], record[@key_proto]) rescue "unknown"
         end
           new_es.add(time, record)
       end
